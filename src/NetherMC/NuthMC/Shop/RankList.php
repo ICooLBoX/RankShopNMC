f91784ae652947738e147bd6fea61f123c524fd4
NuthMC <82439064+nuthcm@users.noreply.github.com>
2021-05-29 10:35:17 +0700

    Create rank

---
+++ src/NetherMC/NuthMC/Shop/RankList.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace NetherMC\NuthMC\Shop;
+
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\commamd\CommandSender;
+
+use NetherMC\NuthMC\Shop\RankPrice;
+use NetherMC\NuthMC\Shop\Normal;
+
+
+
+class RankList {
+	
+	public function RankList($player){
+		$form = new SimpleForm(SimpleForm(function (Player $sender, int $data = null) {
+			$result = $data;
+            if($result === null){
+                return true;
+            }             
+            switch($result){
+            	case 0:
+            Normal::Rank($player);
+                break;
+                case 1:
+              Premium::Rank($player);
+                
+                break;
+            }
+		});
+                
+              
+              
+              $form->addButton("Normal Rank");
+              $form->addButton("Premium Ranks");
+              $form->sendToPlayer($player);
+              return $form;
+            }
+}
