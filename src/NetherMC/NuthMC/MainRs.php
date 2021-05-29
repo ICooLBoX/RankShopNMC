eff3c956cc8a1e38c570dd8dcf932beff71e7f58
NuthMC <82439064+nuthcm@users.noreply.github.com>
2021-05-29 10:05:13 +0700

    Update MainRs.php

--- src/NetherMC/NuthMC/MainRs.php
+++ src/NetherMC/NuthMC/MainRs.php
@@ -2,4 +2,29 @@
 
 namespace NetherMC\NuthMC;
 
-use 
+use pocketmine\Player;
+use pocketmine\Server;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+use Form\FormAPI;
+use Form\SimpleForm;
+use Form\ModalForm;
+
+use NetherMC\NuthMC\Command\Commands;
+
+use Shop\RankList;
+
+use Shop\RankPrice;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+class MainRs extends PluginBase implements Listener {
+	
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getServer()->getCommandMap()->register("/nmcranks", new Commands($this));
+		}
+}
